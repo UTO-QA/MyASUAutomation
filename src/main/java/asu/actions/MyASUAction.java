8e2c237e36a8753de3a61a9f0feb201a90175808
Jahnvi Rai <jrai4@asu.edu>
2019-04-15 13:52:27 -0700

    Added locators

--- src/main/java/asu/actions/MyASUAction.java
+++ src/main/java/asu/actions/MyASUAction.java
@@ -1,6 +1,29 @@
 package asu.actions;
 
 import org.fluentlenium.core.FluentPage;
+import org.fluentlenium.core.domain.FluentWebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class MyASUAction extends FluentPage {
+
+    public List<FluentWebElement> headerList(){
+        return find(".myasu-container .navbar-nav li");
+    }
+
+    @FindBy(css=".fa.fa-home")
+    public FluentWebElement asuHome;
+
+    @FindBy(css="#myasu-tabs-right #utility-area-a .user-name")
+    public FluentWebElement userName;
+
+    public List<FluentWebElement> utilityList(){
+        return find("#myasu-tabs-right #utility-area-b #top-switch span a");
+    }
+
+    @FindBy(css=".resources #resourcesTable")
+    public FluentWebElement resourcesTable;
+
+
 }
--- src/main/java/asu/actions/MyASUHomeAction.java
+++ src/main/java/asu/actions/MyASUHomeAction.java
@@ -1,4 +1,25 @@
 package asu.actions;
 
+import org.fluentlenium.core.domain.FluentWebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MyASUHomeAction extends MyASUAction {
+
+    //Classes Section
+
+    @FindBy(css=".splitcontentleft .box #my_classes")
+    public FluentWebElement myClassSection;
+
+    @FindBy(css="#classes_content #myclasses-sched .box-footer")
+    public FluentWebElement myClassFooterSection;
+
+
+    //Calendar Section
+    @FindBy(css=".splitcontentleft .box #cal_header")
+    public FluentWebElement calendarSection;
+
+
+    @FindBy(css=".splitcontentleft .box #news_header")
+    public FluentWebElement announcementSection;
+
 }
